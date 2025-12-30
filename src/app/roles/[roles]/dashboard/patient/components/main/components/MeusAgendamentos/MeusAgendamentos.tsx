53f16da5db5ba219d5b4f9206f21e60378ab7b15
Lincon <linkon789@gmail.com>
2025-12-30 00:52:06 -0300

    feat: adicionar componente MeusAgendamentos com itens e estilização; refatorar estilos para exibição em quadrados

--- src/app/roles/[roles]/dashboard/patient/components/main/components/MeusAgendamentos/MeusAgendamentos.tsx
+++ src/app/roles/[roles]/dashboard/patient/components/main/components/MeusAgendamentos/MeusAgendamentos.tsx
@@ -1,9 +1,15 @@
 import WelcomeBox from "../WelcomeBox/WelcomeBox";
-import { Plus } from "lucide-react";
+import { Plus, Calendar, CheckCircle, XCircle } from "lucide-react";
 import styles from "./styles/MeusAgendamentos.module.scss";
 import ScheduleAppointmentDialog from "../ScheduleAppointmentDialog/ScheduleAppointmentDialog";
 
 export default function MeusAgendamentos() {
+  const items = [
+    { text: "Próximas", icon: Calendar, number: 5 },
+    { text: "Realizadas", icon: CheckCircle, number: 10 },
+    { text: "Canceladas", icon: XCircle, number: 2 },
+  ];
+
   return (
     <section className={styles["meus-agendamentos"]}>
       <WelcomeBox
@@ -20,6 +26,19 @@ export default function MeusAgendamentos() {
           />
         }
       />
+
+      <div className={styles.squaresContainer}>
+        {items.map((item, index) => {
+          const IconComponent = item.icon;
+          return (
+            <div key={index} className={styles.square}>
+              <IconComponent className={styles.squareIcon} />
+              <span className={styles.squareText}>{item.text}</span>
+              <span className={styles.squareNumber}>{item.number}</span>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
